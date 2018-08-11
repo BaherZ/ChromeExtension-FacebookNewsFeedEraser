5477a5604f5f5d056a1680cce19c6b3837a465b3
Baher <mbaher18@gmail.com>
2018-08-11 09:52:17 +0200

    chrome extension to remove newsfeed from facebook

---
+++ content.js
@@ -0,0 +1,3 @@
+chrome.runtime.sendMessage({todo:"check"})
+$('#content_container').html("<img src='http://1.bp.blogspot.com/-YSvgBHF6BIM/T3rO7MQqnWI/AAAAAAAAAAY/w5uCfd7cZYM/s1600/Capture.JPG'/>")
+
